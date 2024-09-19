e2a697ed7921c821068a3b8fb656cbf266b7b4ad
Depanshu Suyal <dsuya637@mtroyal.ca>
2024-09-19 02:54:50 +0000

    Removing unnecessary headers and comments

--- jdshell.c
+++ jdshell.c
@@ -2,19 +2,16 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include "usr_input.h"
-#include <stdio.h>
-#include <string.h>
+
 int main(char argc, char *argv[]) {
   char usr_inp[MAX_SIZE];
 
   read_usr_inp(usr_inp);
 
-
   while(string_compare(usr_inp, "exit", 4) != TRUE){
       read_usr_inp(usr_inp);
   }
-	
-  /*  flush_std_input_buffer();
-   */
+
+
   return 0;
 }
