9e879840b9190b7df33310b07e9299faa7b5e38a
Depanshu Suyal <dsuya637@mtroyal.ca>
2024-09-25 22:30:18 +0000

    Testing command execution

--- src/jdshell.c
+++ src/jdshell.c
@@ -9,10 +9,12 @@ int main(char argc, char *argv[]) {
   int index = 0;
   do {
     get_command(&command);
-    while(command.argv[index] != NULL) {
+    /*    while(command.argv[index] != NULL) {
 	printf("%s\n", command.argv[index++]);
       }
     index = 0;
+    */
+    execve(command.argv[0], command.argv, NULL);
   }while(string_compare(command.usr_input, "exit", 4) != TRUE);
 
 
