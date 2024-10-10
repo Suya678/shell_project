ab85edb35fe64c8197d023bbb1275aa2252393e5
juan serrato <jserr894@mtroyal.ca>
2024-10-10 20:26:12 +0000

    added test folder for cmd runner

---
+++ tests/test_cmd_runner/tst_cmd_runner.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+#include "parser.h"
+void print_command_args(JOB *job);
+
+
+
+
+int main() {
+
+  printf("TESTING VALIDATE AND PARSE JOB FUNCTIONALITY \nPress enter to continue\n");
+ 
+}
+
